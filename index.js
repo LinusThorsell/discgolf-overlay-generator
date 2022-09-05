680e7d91f7ee537ac67610ef8785d5602cf4e598
Linus Thorsell <thorsell.linus@gmail.com>
2022-09-05 14:11:06 +0200

    added tjing support and player select

--- index.js
+++ index.js
@@ -2,26 +2,11 @@ var front9 = false;
 
 var scorecard = 
 [
-    [
-        2, 4, 4, 3, 3, 4, 4, 6, 4, 
-        4, 3, 3, 5, 4, 3, 3, 3, 3
-    ],
-    [
-        2, 4, 4, 3, 3, 4, 4, 6, 4, 
-        4, 3, 3, 5, 4, 3, 3, 3, 3
-    ],
-    [
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 
-        1, 1, 1, 1, 1, 1, 1, 1, 1
-    ],
-    [
-        2, 4, 4, 3, 3, 4, 4, 6, 4, 
-        4, 3, 3, 5, 4, 3, 3, 3, 3
-    ],
-    [
-        2, 4, 4, 3, 3, 4, 4, 6, 4, 
-        4, 3, 3, 5, 4, 3, 3, 3, 3
-    ],
+    [],
+    [],
+    [],
+    [],
+    []
 ];
 
 var par = 
@@ -32,7 +17,90 @@ var par =
 
 var amountofholesplayed = [0,0,0,0,0];
 
+const mirrorplayercontainer = document.getElementById('playermirror');
+const datatomirror = document.querySelectorAll('.container');
+mirrorplayers()
+
 const alltopcontainers = document.querySelectorAll('.top-container');
+
+var imported_r1;
+
+function updatePlayerList(json) {
+
+    var select_element_1 = document.getElementById("select_player_1");
+    var select_element_2 = document.getElementById("select_player_2");
+    var select_element_3 = document.getElementById("select_player_3");
+    var select_element_4 = document.getElementById("select_player_4");
+    var select_element_5 = document.getElementById("select_player_5");
+    json.forEach((player, index) => {
+        select_element_1.innerHTML += "<option value='" + index + "'>" + player["First name"] + " " + player["Last name"] + "</option>"
+        select_element_2.innerHTML += "<option value='" + index + "'>" + player["First name"] + " " + player["Last name"] + "</option>"
+        select_element_3.innerHTML += "<option value='" + index + "'>" + player["First name"] + " " + player["Last name"] + "</option>"
+        select_element_4.innerHTML += "<option value='" + index + "'>" + player["First name"] + " " + player["Last name"] + "</option>"
+        select_element_5.innerHTML += "<option value='" + index + "'>" + player["First name"] + " " + player["Last name"] + "</option>"
+    });
+}
+
+function confirmPlayers() {
+    
+    console.log(scorecard);
+    
+    var player1 = document.getElementById("select_player_1").value;
+    var player2 = document.getElementById("select_player_2").value;
+    var player3 = document.getElementById("select_player_3").value;
+    var player4 = document.getElementById("select_player_4").value;
+    var player5 = document.getElementById("select_player_5").value;
+
+    scorecard[0] = [];
+    scorecard[1] = [];
+    scorecard[2] = [];
+    scorecard[3] = [];
+    scorecard[4] = [];
+
+    for (i = 1; i <= 18; i++)
+    {
+        scorecard[0].push(imported_r1[player1]['H' + i])
+        scorecard[1].push(imported_r1[player2]['H' + i])
+        scorecard[2].push(imported_r1[player3]['H' + i])
+        scorecard[3].push(imported_r1[player4]['H' + i])
+        scorecard[4].push(imported_r1[player5]['H' + i])
+    }
+
+    console.log(scorecard);
+
+    document.querySelectorAll(".name")[0].innerHTML = imported_r1[player1]["First name"] + " " + imported_r1[player1]["Last name"]
+    document.querySelectorAll(".name")[1].innerHTML = imported_r1[player2]["First name"] + " " + imported_r1[player2]["Last name"]
+    document.querySelectorAll(".name")[2].innerHTML = imported_r1[player3]["First name"] + " " + imported_r1[player3]["Last name"]
+    document.querySelectorAll(".name")[3].innerHTML = imported_r1[player4]["First name"] + " " + imported_r1[player4]["Last name"]
+    document.querySelectorAll(".name")[4].innerHTML = imported_r1[player5]["First name"] + " " + imported_r1[player5]["Last name"]
+    document.querySelectorAll(".name")[5].innerHTML = imported_r1[player1]["First name"] + " " + imported_r1[player1]["Last name"]
+    document.querySelectorAll(".name")[6].innerHTML = imported_r1[player2]["First name"] + " " + imported_r1[player2]["Last name"]
+    document.querySelectorAll(".name")[7].innerHTML = imported_r1[player3]["First name"] + " " + imported_r1[player3]["Last name"]
+    document.querySelectorAll(".name")[8].innerHTML = imported_r1[player4]["First name"] + " " + imported_r1[player4]["Last name"]
+    document.querySelectorAll(".name")[9].innerHTML = imported_r1[player5]["First name"] + " " + imported_r1[player5]["Last name"]
+}
+
+function mirrorplayers() {
+    datatomirror.forEach(container => {
+        mirrorplayercontainer.innerHTML += container.outerHTML;
+        mirrorplayercontainer.innerHTML.innerHTML += container.innerHTML;
+    });
+}
+
+async function handleFileAsync(e) {
+    const file = e.target.files[0];
+    const data = await file.arrayBuffer();
+    /* data is an ArrayBuffer */
+    const workbook = XLSX.read(data);
+
+    /* DO SOMETHING WITH workbook HERE */
+    if (e.target.id === "input_r1_sheet")
+    {
+        imported_r1 = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], false);
+        updatePlayerList(imported_r1)
+    }
+}
+document.getElementById("input_r1_sheet").addEventListener("change", handleFileAsync, false);
 
 function switchnine() {
 
@@ -73,7 +141,18 @@ function downloadURI(uri, name) {
     link.click();
     document.body.removeChild(link);
     delete link;
-  }
+}
+
+function updateHole() {
+    var hole = document.getElementById("info_hole").value;
+    var hardest = document.getElementById("info_hardest").value;
+    var meters = document.getElementById("info_m").value;
+    var feet = document.getElementById("info_ft").value;
+
+    document.getElementById("holelabel").innerHTML = "<p>Hole " + hole + "</p>";
+    document.getElementById("holeinfo").innerHTML = "<p>Par " + hole + "</p><p>" + meters + "m </p><p>" + feet + "ft</p>";
+    document.getElementById("holedifficulty").innerHTML = "<p>" + hardest + "th most difficult</p>";
+}
 
 function updateTotal(player)
 {
@@ -94,7 +173,8 @@ function updateTotal(player)
     console.log(score);
 
     if (score > 0) {score = "+"+score};
-    document.getElementById("total" + player).innerText = score;
+    document.querySelectorAll(".total" + player)[0].innerText = score;
+    document.querySelectorAll(".total" + player)[1].innerText = score;
 
     html2canvas(document.querySelector("#screen"), {backgroundColor:null}).then(canvas => {
         // document.body.appendChild(canvas)
@@ -108,14 +188,14 @@ function triggerShowHoleScore(player, hole)
 {
     amountofholesplayed[player] = hole;
 
-    var element = document.getElementById("s" + player + hole);
+    var element = document.querySelectorAll(".s" + player + hole);
     console.log(element);
 
     var relative = scorecard[player][hole-1] - par[hole-1];
     console.log(relative);
 
-    var backgroundcolor = "brown" // to see if something goes wrong.
-    var color = "aqua"
+    var backgroundcolor = "" // to see if something goes wrong.
+    var color = ""
 
     if (scorecard[player][hole-1] === 1) {
         backgroundcolor = "orange";
@@ -155,10 +235,10 @@ function triggerShowHoleScore(player, hole)
     backgroundcolor = "background-color: " + backgroundcolor + ";";
     color = "color: " + color + ";";
 
-    element.style = color + backgroundcolor;
+    element[0].style = color + backgroundcolor;
+    element[1].style = color + backgroundcolor;
 
     updateTotal(player);
 }
 
 switchnine()
-// triggerShowHoleScore(1, false);
